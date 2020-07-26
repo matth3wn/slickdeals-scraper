94eb23aa6982114ab8646771dbe0b493589f17bb
Matthew Nguyen <matth3wn@gmail.com>
2020-07-25 22:09:08 -0400

    removing more unneeded items

---
+++ README.md
@@ -0,0 +1,10 @@
+# Basic Slickdeals Webscrapper  
+
+### Purpose:  
+You to get all hot/popular deals from slickdeals.net
+
+### To run:  
+> git clone repo  
+> cd slickdeals && cd slickdeals-server && npm install && npm start  
+> cd react-slickdeals && npm i && npm start  
+
--- slickdeals-server/index.js
+++ slickdeals-server/index.js
@@ -4,11 +4,6 @@ const cors = require("cors");
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 const morgan = require("morgan");
-const twilio = require("twilio");
-var accountSid = "AC0d112d72fd13c16f48fb8c5cd216920c"; // Your Account SID from www.twilio.com/console
-var authToken = "aa11964eaed21797f7281ad1a9d41e27";
-var client = new twilio(accountSid, authToken);
-// import someObject from ("../../response.json");
 
 const app = express();
 const port = 4000;
